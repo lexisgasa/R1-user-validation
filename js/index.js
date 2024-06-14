fb2a1904335bc44270300162d9e608526d25a3b7
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-06-14 22:17:09 +0200

    username, password cambiados, regex actualizado

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-const userName = "administrador";
-const password = "Administrador99";
+const username = "administrador";
+const password = "andalucÍa3";
 const confirmPassword = "Administrador99";
 const age = 25;
 
-const passwordHasDigit = password.match(/\d/) !== null;
-const passwordHasCapitalLetter = password.match(/[A-Z]/) !== null;
-const passwordIsValid =
-  password.length >= 8 && passwordHasDigit && passwordHasCapitalLetter;
+const hasDigitInPassword = password.match(/\d/) !== null;
+const hasCapitalLetterInPassword = password.match(/[A-ZÁÉÍÓÚÜÑ]/) !== null;
+const isValidPassword =
+  password.length >= 8 && hasDigitInPassword && hasCapitalLetterInPassword;
 
-console.log(`El nombre de usuario es válido: ${userName.length >= 5}`);
-console.log(`La contraseña es válida: ${passwordIsValid}`);
+console.log(`El nombre de usuario es válido: ${username.length >= 5}`);
+console.log(`La contraseña es válida: ${isValidPassword}`);
 console.log(
   `La repetición de la contraseña es válida: ${password === confirmPassword}`
 );
