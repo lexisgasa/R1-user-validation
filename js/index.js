b2c1c11b5dceeea2ff6096d2f6b374d1600d327e
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-06-15 23:51:24 +0200

    logica separada

--- js/index.js
+++ js/index.js
@@ -3,14 +3,17 @@ const password = "andalucÍa3";
 const confirmPassword = "Administrador99";
 const age = 25;
 
+const isValidUsername = username.length >= 5;
 const hasDigitInPassword = password.match(/\d/) !== null;
-const hasCapitalLetterInPassword = password.match(/[A-ZÁÉÍÓÚÜÑ]/) !== null;
+const hasCapitalLetterInPassword = password.match(/[A-ZÁÉÍÓÚÀÈÌÒÙ]/) !== null;
 const isValidPassword =
   password.length >= 8 && hasDigitInPassword && hasCapitalLetterInPassword;
+const isValidConfirmPassword = password === confirmPassword;
+const iValidAge = age >= 18;
 
-console.log(`El nombre de usuario es válido: ${username.length >= 5}`);
+console.log(`El nombre de usuario es válido: ${isValidUsername}`);
 console.log(`La contraseña es válida: ${isValidPassword}`);
 console.log(
-  `La repetición de la contraseña es válida: ${password === confirmPassword}`
+  `La repetición de la contraseña es válida: ${isValidConfirmPassword}`
 );
-console.log(`Es mayor de edad: ${age >= 18}`);
+console.log(`Es mayor de edad: ${iValidAge}`);
